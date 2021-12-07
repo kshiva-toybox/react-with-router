0c215ba080e7e46f6c6cde333adb77a6fffe3296
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2021-12-07 22:26:44 +0900

    react-helmet-asyncでヘッド情報を書き換え

--- src/components/page/About.tsx
+++ src/components/page/About.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 
 const About: FC = () => {
   return (
     <>
+      <Helmet>
+        <title>About | React App</title>
+      </Helmet>
       <h1>About</h1>
       <Link to="/">Top</Link>
     </>
--- src/components/page/Top.tsx
+++ src/components/page/Top.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
+import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 
 const Top: FC = () => {
   return (
     <>
+      <Helmet>
+        <title>Top | React App</title>
+      </Helmet>
       <h1>TOP</h1>
       <Link to="/about">About</Link>
     </>
