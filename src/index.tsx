51fcee676c34151fad369af2b356fbc88535e53b
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2022-02-03 13:13:49 +0900

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
